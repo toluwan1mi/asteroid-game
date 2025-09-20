35c9b1a6fac44db36480cd0d57734dbf0201d904
toluwan1mi <toluekunola@gmail.com>
2025-09-20 11:57:35 +0100

    added movement functionality to the player

--- player.py
+++ player.py
@@ -23,10 +23,19 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(-dt)
+            self.rotate(-dt) # rotate left
         if keys[pygame.K_d]:
-            self.rotate(dt)
+            self.rotate(dt) # rotate right
+
+        if keys[pygame.K_s]:
+            self.move(-dt) # move down
+        if keys[pygame.K_w]:
+            self.move(dt) # move up
