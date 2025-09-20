a20903f1a4250c0475850c1feb5b0e69ca284d03
toluwan1mi <toluekunola@gmail.com>
2025-09-20 11:16:52 +0100

    added game loop and quit button functionality

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ def main():
     print(f"Screen Width: {SCREEN_WIDTH}")
     print(f"Screen Height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game Loop
+    while True:
+        
+        # Giving the quit button functionality
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black") # Setting background colour to black
+
+        pygame.display.flip() # Refreshing display
+
 
 if __name__ == "__main__":
     main()
